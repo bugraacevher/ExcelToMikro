92ffa6da32e155b600e3fc0a4f2b33a5017998d5
Buğra Cevher <99588506+bugraacevher@users.noreply.github.com>
2022-12-14 17:39:25 +0400

    Add files via upload

---
+++ ExcelToMikro/ExcelToMikro/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace ExcelToMikro
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 86);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Dosya Sec";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(184, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(589, 426);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 120);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(151, 23);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private OpenFileDialog openFileDialog1;
+        private Button button1;
+        private DataGridView dataGridView1;
+        private ComboBox comboBox1;
+    }
+}
---
+++ ExcelToMikro/ExcelToMikro/Form1.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System.Data;
+using DataTable = System.Data.DataTable;
+using ExcelApp = Microsoft.Office.Interop.Excel;
+
+namespace ExcelToMikro
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+            string DosyaYolu;
+            string DosyaAdi;
+            DataTable dt;
+            OpenFileDialog file = new OpenFileDialog();
+            file.Filter = "Excel Dosyası | *.xls; *.xlsx; *.xlsm";
+
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+                DosyaYolu = file.FileName;// seçilen dosyanın tüm yolunu verir
+                DosyaAdi = file.SafeFileName;// seçilen dosyanın adını verir.
+                ExcelApp.Application excelApp = new ExcelApp.Application();
+                if (excelApp == null)
+                { //Excel Yüklümü Kontrolü Yapılmaktadır.
+                    MessageBox.Show("Excel yüklü değil.");
+                    return;
+                }
+                //Excel Dosyası Açılıyor.
+                ExcelApp.Workbook excelBook = excelApp.Workbooks.Open(DosyaYolu);
+                //Excel Dosyasının Sayfası Seçilir.
+                ExcelApp._Worksheet excelSheet = excelBook.Sheets[1];
+                //Excel Dosyasının ne kadar satır ve sütun kaplıyorsa tüm alanları alır.
+                ExcelApp.Range excelRange = excelSheet.UsedRange;
+                int satirSayisi = excelRange.Rows.Count; //Sayfanın satır sayısını alır.
+                int sutunSayisi = excelRange.Columns.Count;//Sayfanın sütun sayısını alır.
+                dt = ToDataTable(excelRange, satirSayisi, sutunSayisi);
+                dataGridView1.DataSource = dt;
+                dataGridView1.Refresh();
+                //Okuduktan Sonra Excel Uygulamasını Kapatıyoruz.
+                excelApp.Quit();
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(excelApp);
+            }
+            else
+            {
+                MessageBox.Show("Dosya Seçilemedi.");
+            }
+
+        }
+        public DataTable ToDataTable(ExcelApp.Range range, int rows, int cols)
+        {
+            DataTable table = new DataTable();
+            for (int i = 1; i <= rows; i++)
+            {
+                if (i == 1)
+                { // ilk satırı Sutun Adları olarak kullanıldığından
+                  // bunları Sutün Adları Olarak Kaydediyoruz.
+                    for (int j = 1; j <= cols; j++)
+                    {
+                        //Sütunların içeriği boş mu kontrolü yapılmaktadır.
+                        if (range.Cells[i, j] != null && range.Cells[i, j].Value2 != null)
+                            table.Columns.Add(range.Cells[i, j].Value2.ToString());
+                        else //Boş olduğunda Kaçınsı Sutünsa Adı veriliyor.
+                            table.Columns.Add(j.ToString() + ".Sütun");
+                    }
+                    continue;
+                }
+                //Yukarıda Sütunlar eklendi
+                // onun şemasına göre yeni bir satır oluşturuyoruz. 
+                //Okunan verileri yan yana sıralamak için
+                var yeniSatir = table.NewRow();
+                for (int j = 1; j <= cols; j++)
+                {
+                    //if (j == 1)
+                    //{
+                    //    range.Cells[i, j].Style.Numberformat.Format = System.Globalization.DateTimeFormatInfo.CurrentInfo.ShortDatePattern;
+                    //}
+                    //Sütunların içeriği boş mu kontrolü yapılmaktadır.
+                    if (range.Cells[i, j] != null && range.Cells[i, j].Value2 != null)
+                    {
+                        yeniSatir[j - 1] = range.Cells[i, j].Value2.ToString();
+                    }
+                    else
+                    {
+                        yeniSatir[j - 1] = String.Empty;
+                    } // İçeriği boş hücrede hata vermesini önlemek için
+                }
+                table.Rows.Add(yeniSatir);
+            }
+            return table;
+        }
+    }
+}
